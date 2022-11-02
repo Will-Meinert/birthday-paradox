0e52bde2f08d7066ba63dc6d4d2ba4a18ed59387
UnfedNintendogs <62534134+UnfedNintendogs@users.noreply.github.com>
2022-11-02 17:55:20 +1100

    Initial commit

---
+++ arctan.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def birthday_paradox(n):
+	probability = 1
+	for i in range(1, n):
+		probability = probability * (365 - i) / 365
+	return 1 - probability
+
+
+# arctan
+# error = 1.5972928762910859
+a = 12.977
+b = 10.285
+c = 21.295
+d = 9.33
+e = 95
+f = 0.447
+def arctan(x, original: bool = False):
+	if original:
+		return (13/(10*np.pi))*np.arctan((x-23)*9/100) + 0.5
+	else:
+		return (a/(b*np.pi))*np.arctan((x-c)*d/e) + f
+
+
+data1 = [birthday_paradox(i) for i in range(0, 100)]
+data2 = [arctan(i) for i in range(0, 100)]
+data3 = [arctan(i, True) for i in range(0, 100)]
+
+#calculate error of data 2 and data 3 to the original data
+error = 0
+for i in range(0, 100):
+	error += abs(data1[i] - data2[i])
+print('dataset 2 error: ' + str(error))
+error = 0
+for i in range(0, 100):
+	error += abs(data1[i] - data3[i])
+print('dataset 3 error: ' + str(error))
+
+
+
+plt.plot(data1)
+plt.plot(data2)
+plt.plot(data3)
+plt.legend(['Birthday Paradox', 'Arctan', 'Original Arctan'])
+plt.show()
+
+#show both data sets on the same plot with different lines
+
---
+++ calculate.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def birthday_paradox(n):
+	probability = 1
+	for i in range(1, n):
+		probability = probability * (365 - i) / 365
+	return 1 - probability
+# arctan
+
+def arctan(x, a, b, c, d, e, f):
+	try :
+		return (a/(b*np.pi))*np.arctan((x-c)*d/e) + f
+	except ZeroDivisionError:
+		return 0
+
+
+#generate random flot in range with 2 decimals of precision
+
+def random_float(start, end):
+	return round(np.random.uniform(start, end), 3)
+
+
+data1 = [birthday_paradox(i) for i in range(0, 100)]
+best_error = 0
+while 1: 
+	a = random_float(10, 15)
+	b = random_float(8, 12)
+	c = random_float(20, 25)
+	d = random_float(7, 15)
+	e = np.random.randint(1, 100)
+	f = random_float(0, 1)
+	data2 = [arctan(i, a, b, c, d, e, f) for i in range(0, 100)]
+	# find error between data points
+	error = 0
+	for i in range(0, 100):
+		error += abs(data1[i] - data2[i])
+	if error < best_error or best_error == 0:
+	#if error is less than last error, save the values
+		best_error = error
+		print('new best error: ' + str(best_error) + ' with a = ' + str(a) + ', b = ' + str(b) + ', c = ' + str(c) + ', d = ' + str(d) + ', e = ' + str(e) + ', f = ' + str(f))
+
+
+
+
