10a71a43689a8bfdf32a18c73541dc6d5a515e21
UnfedNintendogs <62534134+UnfedNintendogs@users.noreply.github.com>
2022-11-03 12:55:33 +1100

    cleanup imports and format

--- final.py
+++ final.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import json
+
 
 def birthday_paradox(n):
-	probability = 1
-	for i in range(1, n):
-		probability = probability * (365 - i) / 365
-	return 1 - probability
+    probability = 1
+    for i in range(1, n):
+        probability = probability * (365 - i) / 365
+    return 1 - probability
 
 
 # arctan values
@@ -18,23 +18,23 @@ d = 7.599
 e = 80.128
 f = 0.44
 
-def arctan(x):
-	return (a/(b*np.pi))*np.arctan((x-c)*d/e) + f
 
-#calculate data
+def arctan(x):
+    return (a/(b*np.pi))*np.arctan((x-c)*d/e) + f
+
+
+# calculate data
 data1 = [birthday_paradox(i) for i in range(0, 100)]
 data2 = [arctan(i) for i in range(0, 100)]
 
-#calculate error of data 2 to the original data
+# calculate error of data 2 to the original data
 error = 0
 for i in range(0, 100):
-	error += abs(data1[i] - data2[i])
+    error += abs(data1[i] - data2[i])
 print('dataset 2 error: ' + str(error))
 
-#show data
+# show data
 plt.plot(data1)
 plt.plot(data2)
 plt.legend(['Birthday Paradox', 'Arctan'])
 plt.show()
-
-
