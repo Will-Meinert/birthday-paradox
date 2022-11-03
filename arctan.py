c23bbfadfbc1972ef00ddb308afd924fee9d92cb
UnfedNintendogs <62534134+UnfedNintendogs@users.noreply.github.com>
2022-11-03 11:54:59 +1100

    Permutation and line toggling

--- arctan.py
+++ arctan.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import json
+import math
+
 
 def birthday_paradox(n):
 	probability = 1
@@ -8,7 +10,9 @@ def birthday_paradox(n):
 		probability = probability * (365 - i) / 365
 	return 1 - probability
 
-
+def birthday_paradox_permute(n):
+	return 1 - ((math.floor(math.factorial(365)/math.factorial(365 - n))/365**n))
+	
 # arctan values
 # read arctan values from json file
 with open('values.json', 'r') as val:
@@ -30,6 +34,7 @@ def arctan(x, original: bool = False):
 data1 = [birthday_paradox(i) for i in range(0, 100)]
 data2 = [arctan(i) for i in range(0, 100)]
 data3 = [arctan(i, True) for i in range(0, 100)]
+data4 = [birthday_paradox_permute(i) for i in range(0, 100)]
 
 #calculate error of data 2 and data 3 to the original data
 error = 0
@@ -41,12 +46,32 @@ for i in range(0, 100):
 	error += abs(data1[i] - data3[i])
 print('dataset 3 error: ' + str(error))
 
-
+fig, ax = plt.subplots()
 #show data
-plt.plot(data1)
-plt.plot(data2)
-plt.plot(data3)
-plt.legend(['Birthday Paradox', 'Arctan', 'Original Arctan'])
+line1, = ax.plot(data1)
+line2, = ax.plot(data2)
+line3, = ax.plot(data3)
+line4, = ax.plot(data4)
+
+lines = [line1, line2, line3, line4]
+lined = {}  # Will map legend lines to original lines.
+leg = plt.legend(['Birthday Paradox', 'Arctan', 'Original Arctan', 'Birthday Paradox Permute'])
+for legline, origline in zip(leg.get_lines(), lines):
+    legline.set_picker(True)  # Enable picking on the legend line.
+    lined[legline] = origline
+def on_pick(event):
+    # On the pick event, find the original line corresponding to the legend
+    # proxy line, and toggle its visibility.
+    legline = event.artist
+    origline = lined[legline]
+    visible = not origline.get_visible()
+    origline.set_visible(visible)
+    # Change the alpha on the line in the legend so we can see what lines
+    # have been toggled.
+    legline.set_alpha(1.0 if visible else 0.2)
+    fig.canvas.draw()
+
+fig.canvas.mpl_connect('pick_event', on_pick)
 plt.show()
 
 
